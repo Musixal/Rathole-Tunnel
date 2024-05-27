985fda45987224f36609c8f5bffbb66d3384b62c
MusiX <124933548+Musixal@users.noreply.github.com>
2024-05-27 18:05:10 +0330

    Update rathole.sh

--- rathole.sh
+++ rathole.sh
@@ -707,7 +707,7 @@ ports_monitor_menu(){
     clear
     # Prompt user to choose a option
     echo -e "Select the option you want to do:\n"
-    echo -e "${CYAN}1. Add ports to iptables rules${NC}"
+    echo -e "${CYAN}1. Add ports for monitoring traffic${NC}"
     echo -e "${GREEN}2. View traffic usage${NC}"
     echo -e "${RED}3. Remove iptables rules${NC}"
     echo ''
