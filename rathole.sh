a9dd32a792fe5a56d38f9336565e33c048bf8815
MusiX <124933548+Musixal@users.noreply.github.com>
2024-06-29 15:41:48 +0330

    Update rathole.sh
    add menu for ubuntu optimizations

--- rathole.sh
+++ rathole.sh
@@ -162,7 +162,7 @@ ____________ _/  |_|  |__   ____ |  |   ____
 EOF
     echo -e "${NC}${GREEN}"
     echo -e "${YELLOW}High-performance reverse tunnel${GREEN}"
-    echo -e "Version: ${YELLOW}v1.3.1${GREEN}"
+    echo -e "Version: ${YELLOW}v1.3.2${GREEN}"
     echo -e "Developer: ${YELLOW}Musixal${GREEN}"
     echo -e "Github: ${YELLOW}github.com/Musixal/Rathole-Tunnel${GREEN}"
     echo -e "Telegram Channel: ${YELLOW}@Gozar_Xray${NC}"
@@ -1024,6 +1024,405 @@ fi
 
 }
 
+
+
+
+
+
+# _________________________ HAWSHEMI SCRIPT OPT FOR UBUNTU _________________________
+
+# Declare Paths & Settings.
+SYS_PATH="/etc/sysctl.conf"
+PROF_PATH="/etc/profile"
+
+
+# Ask Reboot
+ask_reboot() {
+    echo -ne "${YELLOW}Reboot now? (Recommended) (y/n): ${NC}"
+    while true; do
+        read choice
+        echo 
+        if [[ "$choice" == 'y' || "$choice" == 'Y' ]]; then
+            sleep 0.5
+            reboot
+            exit 0
+        fi
+        if [[ "$choice" == 'n' || "$choice" == 'N' ]]; then
+            break
+        fi
+    done
+}
+# SYSCTL Optimization
+sysctl_optimizations() {
+    ## Make a backup of the original sysctl.conf file
+    cp $SYS_PATH /etc/sysctl.conf.bak
+
+    echo 
+    echo -e "${YELLOW}Default sysctl.conf file Saved. Directory: /etc/sysctl.conf.bak${NC}"
+    echo 
+    sleep 1
+
+    echo 
+    echo -e  "${YELLOW}Optimizing the Network...${NC}"
+    echo 
+    sleep 0.5
+
+    sed -i -e '/fs.file-max/d' \
+        -e '/net.core.default_qdisc/d' \
+        -e '/net.core.netdev_max_backlog/d' \
+        -e '/net.core.optmem_max/d' \
+        -e '/net.core.somaxconn/d' \
+        -e '/net.core.rmem_max/d' \
+        -e '/net.core.wmem_max/d' \
+        -e '/net.core.rmem_default/d' \
+        -e '/net.core.wmem_default/d' \
+        -e '/net.ipv4.tcp_rmem/d' \
+        -e '/net.ipv4.tcp_wmem/d' \
+        -e '/net.ipv4.tcp_congestion_control/d' \
+        -e '/net.ipv4.tcp_fastopen/d' \
+        -e '/net.ipv4.tcp_fin_timeout/d' \
+        -e '/net.ipv4.tcp_keepalive_time/d' \
+        -e '/net.ipv4.tcp_keepalive_probes/d' \
+        -e '/net.ipv4.tcp_keepalive_intvl/d' \
+        -e '/net.ipv4.tcp_max_orphans/d' \
+        -e '/net.ipv4.tcp_max_syn_backlog/d' \
+        -e '/net.ipv4.tcp_max_tw_buckets/d' \
+        -e '/net.ipv4.tcp_mem/d' \
+        -e '/net.ipv4.tcp_mtu_probing/d' \
+        -e '/net.ipv4.tcp_notsent_lowat/d' \
+        -e '/net.ipv4.tcp_retries2/d' \
+        -e '/net.ipv4.tcp_sack/d' \
+        -e '/net.ipv4.tcp_dsack/d' \
+        -e '/net.ipv4.tcp_slow_start_after_idle/d' \
+        -e '/net.ipv4.tcp_window_scaling/d' \
+        -e '/net.ipv4.tcp_adv_win_scale/d' \
+        -e '/net.ipv4.tcp_ecn/d' \
+        -e '/net.ipv4.tcp_ecn_fallback/d' \
+        -e '/net.ipv4.tcp_syncookies/d' \
+        -e '/net.ipv4.udp_mem/d' \
+        -e '/net.ipv6.conf.all.disable_ipv6/d' \
+        -e '/net.ipv6.conf.default.disable_ipv6/d' \
+        -e '/net.ipv6.conf.lo.disable_ipv6/d' \
+        -e '/net.unix.max_dgram_qlen/d' \
+        -e '/vm.min_free_kbytes/d' \
+        -e '/vm.swappiness/d' \
+        -e '/vm.vfs_cache_pressure/d' \
+        -e '/net.ipv4.conf.default.rp_filter/d' \
+        -e '/net.ipv4.conf.all.rp_filter/d' \
+        -e '/net.ipv4.conf.all.accept_source_route/d' \
+        -e '/net.ipv4.conf.default.accept_source_route/d' \
+        -e '/net.ipv4.neigh.default.gc_thresh1/d' \
+        -e '/net.ipv4.neigh.default.gc_thresh2/d' \
+        -e '/net.ipv4.neigh.default.gc_thresh3/d' \
+        -e '/net.ipv4.neigh.default.gc_stale_time/d' \
+        -e '/net.ipv4.conf.default.arp_announce/d' \
+        -e '/net.ipv4.conf.lo.arp_announce/d' \
+        -e '/net.ipv4.conf.all.arp_announce/d' \
+        -e '/kernel.panic/d' \
+        -e '/vm.dirty_ratio/d' \
+        -e '/^#/d' \
+        -e '/^$/d' \
+        "$SYS_PATH"
+
+
+    ## Add new parameteres.
+
+cat <<EOF >> "$SYS_PATH"
+
+
+################################################################
+################################################################
+
+
+# /etc/sysctl.conf
+# These parameters in this file will be added/updated to the sysctl.conf file.
+# Read More: https://github.com/hawshemi/Linux-Optimizer/blob/main/files/sysctl.conf
+
+
+## File system settings
+## ----------------------------------------------------------------
+
+# Set the maximum number of open file descriptors
+fs.file-max = 67108864
+
+
+## Network core settings
+## ----------------------------------------------------------------
+
+# Specify default queuing discipline for network devices
+net.core.default_qdisc = fq_codel
+
+# Configure maximum network device backlog
+net.core.netdev_max_backlog = 32768
+
+# Set maximum socket receive buffer
+net.core.optmem_max = 262144
+
+# Define maximum backlog of pending connections
+net.core.somaxconn = 65536
+
+# Configure maximum TCP receive buffer size
+net.core.rmem_max = 33554432
+
+# Set default TCP receive buffer size
+net.core.rmem_default = 1048576
+
+# Configure maximum TCP send buffer size
+net.core.wmem_max = 33554432
+
+# Set default TCP send buffer size
+net.core.wmem_default = 1048576
+
+
+## TCP settings
+## ----------------------------------------------------------------
+
+# Define socket receive buffer sizes
+net.ipv4.tcp_rmem = 16384 1048576 33554432
+
+# Specify socket send buffer sizes
+net.ipv4.tcp_wmem = 16384 1048576 33554432
+
+# Set TCP congestion control algorithm to BBR
+net.ipv4.tcp_congestion_control = bbr
+
+# Configure TCP FIN timeout period
+net.ipv4.tcp_fin_timeout = 25
+
+# Set keepalive time (seconds)
+net.ipv4.tcp_keepalive_time = 1200
+
+# Configure keepalive probes count and interval
+net.ipv4.tcp_keepalive_probes = 7
+net.ipv4.tcp_keepalive_intvl = 30
+
+# Define maximum orphaned TCP sockets
+net.ipv4.tcp_max_orphans = 819200
+
+# Set maximum TCP SYN backlog
+net.ipv4.tcp_max_syn_backlog = 20480
+
+# Configure maximum TCP Time Wait buckets
+net.ipv4.tcp_max_tw_buckets = 1440000
+
+# Define TCP memory limits
+net.ipv4.tcp_mem = 65536 1048576 33554432
+
+# Enable TCP MTU probing
+net.ipv4.tcp_mtu_probing = 1
+
+# Define minimum amount of data in the send buffer before TCP starts sending
+net.ipv4.tcp_notsent_lowat = 32768
+
+# Specify retries for TCP socket to establish connection
+net.ipv4.tcp_retries2 = 8
+
+# Enable TCP SACK and DSACK
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_dsack = 1
+
+# Disable TCP slow start after idle
+net.ipv4.tcp_slow_start_after_idle = 0
+
+# Enable TCP window scaling
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_adv_win_scale = -2
+
+# Enable TCP ECN
+net.ipv4.tcp_ecn = 1
+net.ipv4.tcp_ecn_fallback = 1
+
+# Enable the use of TCP SYN cookies to help protect against SYN flood attacks
+net.ipv4.tcp_syncookies = 1
+
+
+## UDP settings
+## ----------------------------------------------------------------
+
+# Define UDP memory limits
+net.ipv4.udp_mem = 65536 1048576 33554432
+
+
+## IPv6 settings
+## ----------------------------------------------------------------
+
+# Enable IPv6
+net.ipv6.conf.all.disable_ipv6 = 0
+
+# Enable IPv6 by default
+net.ipv6.conf.default.disable_ipv6 = 0
+
+# Enable IPv6 on the loopback interface (lo)
+net.ipv6.conf.lo.disable_ipv6 = 0
+
+
+## UNIX domain sockets
+## ----------------------------------------------------------------
+
+# Set maximum queue length of UNIX domain sockets
+net.unix.max_dgram_qlen = 256
+
+
+## Virtual memory (VM) settings
+## ----------------------------------------------------------------
+
+# Specify minimum free Kbytes at which VM pressure happens
+vm.min_free_kbytes = 65536
+
+# Define how aggressively swap memory pages are used
+vm.swappiness = 10
+
+# Set the tendency of the kernel to reclaim memory used for caching of directory and inode objects
+vm.vfs_cache_pressure = 250
+
+
+## Network Configuration
+## ----------------------------------------------------------------
+
+# Configure reverse path filtering
+net.ipv4.conf.default.rp_filter = 2
+net.ipv4.conf.all.rp_filter = 2
+
+# Disable source route acceptance
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+
+# Neighbor table settings
+net.ipv4.neigh.default.gc_thresh1 = 512
+net.ipv4.neigh.default.gc_thresh2 = 2048
+net.ipv4.neigh.default.gc_thresh3 = 16384
+net.ipv4.neigh.default.gc_stale_time = 60
+
+# ARP settings
+net.ipv4.conf.default.arp_announce = 2
+net.ipv4.conf.lo.arp_announce = 2
+net.ipv4.conf.all.arp_announce = 2
+
+# Kernel panic timeout
+kernel.panic = 1
+
+# Set dirty page ratio for virtual memory
+vm.dirty_ratio = 20
+
+
+################################################################
+################################################################
+
+
+EOF
+
+    sudo sysctl -p
+    
+    echo 
+    echo -e "${GREEN}Network is Optimized.${NC}"
+    echo 
+    sleep 0.5
+}
+
+
+# System Limits Optimizations
+limits_optimizations() {
+    echo
+    echo -e "${YELLOW}Optimizing System Limits...${NC}"
+    echo 
+    sleep 0.5
+
+    ## Clear old ulimits
+    sed -i '/ulimit -c/d' $PROF_PATH
+    sed -i '/ulimit -d/d' $PROF_PATH
+    sed -i '/ulimit -f/d' $PROF_PATH
+    sed -i '/ulimit -i/d' $PROF_PATH
+    sed -i '/ulimit -l/d' $PROF_PATH
+    sed -i '/ulimit -m/d' $PROF_PATH
+    sed -i '/ulimit -n/d' $PROF_PATH
+    sed -i '/ulimit -q/d' $PROF_PATH
+    sed -i '/ulimit -s/d' $PROF_PATH
+    sed -i '/ulimit -t/d' $PROF_PATH
+    sed -i '/ulimit -u/d' $PROF_PATH
+    sed -i '/ulimit -v/d' $PROF_PATH
+    sed -i '/ulimit -x/d' $PROF_PATH
+    sed -i '/ulimit -s/d' $PROF_PATH
+
+
+    ## Add new ulimits
+    ## The maximum size of core files created.
+    echo "ulimit -c unlimited" | tee -a $PROF_PATH
+
+    ## The maximum size of a process's data segment
+    echo "ulimit -d unlimited" | tee -a $PROF_PATH
+
+    ## The maximum size of files created by the shell (default option)
+    echo "ulimit -f unlimited" | tee -a $PROF_PATH
+
+    ## The maximum number of pending signals
+    echo "ulimit -i unlimited" | tee -a $PROF_PATH
+
+    ## The maximum size that may be locked into memory
+    echo "ulimit -l unlimited" | tee -a $PROF_PATH
+
+    ## The maximum memory size
+    echo "ulimit -m unlimited" | tee -a $PROF_PATH
+
+    ## The maximum number of open file descriptors
+    echo "ulimit -n 1048576" | tee -a $PROF_PATH
+
+    ## The maximum POSIX message queue size
+    echo "ulimit -q unlimited" | tee -a $PROF_PATH
+
+    ## The maximum stack size
+    echo "ulimit -s -H 65536" | tee -a $PROF_PATH
+    echo "ulimit -s 32768" | tee -a $PROF_PATH
+
+    ## The maximum number of seconds to be used by each process.
+    echo "ulimit -t unlimited" | tee -a $PROF_PATH
+
+    ## The maximum number of processes available to a single user
+    echo "ulimit -u unlimited" | tee -a $PROF_PATH
+
+    ## The maximum amount of virtual memory available to the process
+    echo "ulimit -v unlimited" | tee -a $PROF_PATH
+
+    ## The maximum number of file locks
+    echo "ulimit -x unlimited" | tee -a $PROF_PATH
+
+
+    echo 
+    echo -e "${GREEN}System Limits are Optimized.${NC}"
+    echo 
+    sleep 0.5
+}
+
+
+# _________________________ END OF HAWSHEMI SCRIPT OPT FOR UBUNTU _________________________
+
+hawshemi_script(){
+clear
+
+echo -e "${MAGENTA}Special thanks to Hawshemi, the author of optimizer script...${NC}"
+sleep 2
+# Get the operating system name
+os_name=$(lsb_release -is)
+
+echo -e 
+# Check if the operating system is Ubuntu
+if [ "$os_name" == "Ubuntu" ]; then
+  echo -e "${GREEN}The operating system is Ubuntu.${NC}"
+  sleep 1
+else
+  echo -e "${RED} The operating system is not Ubuntu.${NC}"
+  sleep 2
+  return
+fi
+
+
+sysctl_optimizations
+limits_optimizations
+ask_reboot
+read -p "Press Enter to continue..."
+}
+
+
+
 # Color codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -1047,8 +1446,9 @@ display_menu() {
     echo -e "6. Port traffic monitoring"
     echo -e "7. Change security token (Advanced)"
  	echo -e "8. Install Rathole core"
- 	echo -e "9. Install & Update script"
-    echo -e "10. Exit"
+ 	echo -e "9. Optimize Network & System Limits (Ubuntu)"
+ 	echo -e "10. Install & Update script"
+    echo -e "0. Exit"
     echo ''
     echo "-------------------------------"
 }
@@ -1065,8 +1465,9 @@ read_option() {
         6) ports_monitor_menu ;;
         7) check_security_token ;;
         8) download_and_extract_rathole ;;
-        9) update_script ;;
-        10) exit 0 ;;
+        9) hawshemi_script ;;
+        10) update_script ;;
+        0) exit 0 ;;
         *) echo -e "${RED}Invalid option!${NC}" && sleep 1 ;;
     esac
 }
